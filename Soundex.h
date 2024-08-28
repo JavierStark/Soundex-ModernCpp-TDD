6cc7c386430f5304b8fa02916c5e53da4ed9a077
JavierStark <javiertorralbocortes@gmail.com>
2024-08-28 12:26:41 +0200

    refactor: extract maxCodeLength constant and encodedDigit

--- Soundex.h
+++ Soundex.h
@@ -7,23 +7,29 @@
 
 #include <string>
 
+
 class Soundex{
 public:
     std::string encode(const std::string& word)const{
         return zeroPad(head(word) + encodedDigits(word));
     }
 
+private:
+    static const size_t MaxCodeLength {4};
+
     std::string head(const std::string& word) const {
         return word.substr(0, 1);
     }
 
     std::string encodedDigits(const std::string& word) const{
-        return word.length() > 1 ? "1" : "";
+        return word.length() > 1 ? encodedDigit() : "";
+    }
+    std::string encodedDigit() const{
+        return "1";
     }
 
-private:
     std::string zeroPad(const std::string& word) const{
-        auto zerosNeeded = 4 - word.length();
+        auto zerosNeeded = MaxCodeLength - word.length();
 
         return word + std::string(zerosNeeded, '0');
     }
