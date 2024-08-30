13148e34180c5dc64683b9ec5dd0320ec8975674
JavierStark <javiertorralbocortes@gmail.com>
2024-08-30 20:48:14 +0200

    refactor: extract two phases at encodedDigits

--- Soundex.h
+++ Soundex.h
@@ -58,8 +58,17 @@ private:
     std::string encodedDigits(const std::string& word) const{
         std::string encoding;
 
-        encoding += encodedDigit(word.front());
+        encodeHead(encoding, word);
+        encodeTail(encoding, word);
 
+        return encoding;
+    }
+
+    void encodeHead(std::string &encoding, const std::string &word) const {
+        encoding += encodedDigit(word.front());
+    }
+
+    void encodeTail(std::string &encoding, const std::string &word) const {
         for(auto c : tail(word)) {
             if(isComplete(encoding)) break;
 
@@ -68,8 +77,6 @@ private:
             if(digit != NotADigit && digit != lastDigit(encoding))
                 encoding += encodedDigit(c);
         }
-
-        return encoding;
     }
 
     static bool isComplete(const std::string &encoding) { return encoding.length() == MaxCodeLength; }
